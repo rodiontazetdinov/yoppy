30bf9f1b1bebf10bbd2453de3fbb0c3ae56ce7d8
rodiontazetdinov <rodiontazetdinov@yandex.ru>
2025-01-06 14:52:48 +0300

    новая попытка

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  trailingSlash: true,
+  eslint: {
+    // Предупреждения ESLint не будут считаться ошибками при сборке
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Ошибки типизации не будут считаться ошибками при сборке
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
